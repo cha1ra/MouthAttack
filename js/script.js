b0bc2bc99a0fbc0d5078c64eb9155cda924cdd18
cha1ra <cha1ra.garnet@gmail.com>
2018-11-07 15:05:14 +0900

    Create Super Class

--- js/script.js
+++ js/script.js
@@ -5,57 +5,82 @@ Animation
 const testTextArr = ["魑魅魍魎", "死屍累々", "凱旋帰国"];
 
 class GameObject {
-    
-}
-
-
-
-class Player {
-    constructor(posX,posY,imgSrc,canvasId){
+    constructor(posX,posY,canvasId){
         this.posX = posX;
         this.posY = posY;
-        this.imgSrc = imgSrc;
+        this.objW = 0;
+        this.objH = 0;
         this.thisCanvas = document.querySelector(canvasId);
         this.ctx = this.thisCanvas.getContext('2d');
-    }
-
-    draw(){
-        var img = new Image();//dummy
-        img.src = this.imgSrc;
-        img.onload = () => {
-            this.ctx.clearRect(this.posX,this.posY,img.width,img.height);
-            this.ctx.drawImage(img,this.posX,this.posY);
-        }
     }
 
     move(x,y){
         this.posX += x;
         this.posY += y;
     }
-}
 
-class TextBall{
-    constructor(posX,posY,text,canvasId){
-        this.posX = posX;
-        this.posY = posY;
-        this.text = text;
-        this.thisCanvas = document.querySelector(canvasId);
-        this.ctx = this.thisCanvas.getContext('2d');
+    clear(){
+        this.ctx.clearRect(this.posX,this.posY,this.objW,this.objH);
     }
 
+    setObjWH(w, h){
+        this.objW = w;
+        this.objH = h;
+    }
+}
+
+class Player extends GameObject{
+    constructor(posX,posY,imgSrc,canvasId){
+        super(posX,posY,canvasId);
+        this.imgSrc = imgSrc;
+    }
+
+    draw(){
+        var img = new Image();
+        img.src = this.imgSrc;
+        super.setObjWH(img.width, img.height);
+        img.onload = () => {
+            //this.ctx.clearRect(this.posX,this.posY,img.width,img.height);
+            this.ctx.drawImage(img,this.posX,this.posY);
+        }
+    }
 
 }
 
+class TextBall extends GameObject{
+    constructor(posX,posY,text,canvasId){
+        super(posX,posY,canvasId);
+        this.text = text;
+        this.fontSize = 18;
+    }
+
+    draw(){
+        this.setTextStyle();
+        const textWidth = this.ctx.measureText(this.text);
+        console.log(textWidth.width, this.fontSize);
+        this.ctx.fillText(this.text,this.posX,this.posY);
+    }
+    
+    setTextStyle(){
+        this.ctx.font = this.fontSize;
+        this.ctx.fillStyle = '#ff0000';
+    }
+
+}
 
 //Initialize
 const myPlayer = new Player(0,0,'./img/ksg.png','#myCanvas');
+const text = new TextBall(400,100,'魑魅魍魎','#myCanvas');
 console.log(myPlayer.posX);
 
 //Animation
 setInterval(function(){
-
-    myPlayer.draw();
+    myPlayer.clear();
     myPlayer.move(5,5);
+    myPlayer.draw();
+    text.clear();
+    text.move(0,2);
+    text.draw();
     
 },50)
 
